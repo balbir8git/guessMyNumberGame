5b41a6cd7c7f3ef7de90f2a2aa11b8a3225c3267
Singhbalbir <balbir4email@gmail.com>
2018-12-29 19:45:54 +0530

    script: got the clicked element

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-for (var i=1; i<=100; i ++) {
-    const btn
+var container = document.querySelector('#container');
+container.addEventListener('click', game, false);
+
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random()*3 + 1);
+    return randomNumber;
 }
+
+function game(e) {
+    if(e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        alert("hello " + clickedItem);
+    }
+    // e.stopPropagation();
+}
+
